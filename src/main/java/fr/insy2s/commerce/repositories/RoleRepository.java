de277a14ede637acb29d15813cc341630c04af55
Hocine-bedrouni <Hocinebedrouni80@gmail.com>
2023-01-18 21:55:08 +0100

    ok

--- src/main/java/fr/insy2s/commerce/repositories/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package fr.insy2s.commerce.repositories;
-
-import fr.insy2s.commerce.models.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-
-    Role save(String role_name);
-}
